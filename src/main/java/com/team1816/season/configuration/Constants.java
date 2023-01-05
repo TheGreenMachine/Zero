6f272dcd154ebde3b73ddd434fd4bc6a9d566fc8
Keerthi Kaashyap <keerthik23939@isd273.org>
2023-01-05 12:11:04 -0600

    update beta bindings and pre-release vendordeps

--- src/main/java/com/team1816/season/configuration/Constants.java
+++ src/main/java/com/team1816/season/configuration/Constants.java
@@ -44,7 +44,7 @@ public class Constants {
     public static final double kHeightFromCamToHub =
         kTargetHeight - kCameraMountingHeight; // inches
     public static final double kCameraMountingAngleY = 20; // degrees
-    public static final double kTurretZedRadius = Units.inchesToMeters(7); // meters TODO: VERIFY VALUE
+    public static final double kTurretZedRadius = Units.inchesToMeters(7); // meters
     public static final double fieldCenterY = 8.23 / 2.0;
     public static final double fieldCenterX = 16.46 / 2.0;
     public static final Pose2d fieldCenterPose = new Pose2d(
