e7a55dbda2b8bce88fa715fcf37aab25be394a4f
Elena Loucks <elenaloucks@Elenas-MacBook-Air.local>
2023-01-11 17:46:54 -0600

    fix trajectory to point

--- src/main/java/com/team1816/season/auto/actions/TrajectoryToPointAction.java
+++ src/main/java/com/team1816/season/auto/actions/TrajectoryToPointAction.java
@@ -1,5 +1,7 @@
 package com.team1816.season.auto.actions;
 
+import com.team1816.lib.auto.actions.TrajectoryAction;
+import com.team1816.lib.auto.modes.AutoMode;
 import com.team1816.lib.auto.paths.AutoPath;
 import com.team1816.lib.subsystems.drive.Drive;
 import com.team1816.season.states.RobotState;
@@ -16,50 +18,33 @@ public class TrajectoryToPointAction extends AutoPath {
 
     private static RobotState robotState;
 
-    private static TrajectoryAction trajectory;
+    private static TrajectoryAction trajectoryAction;
+
+    private static List<TrajectoryAction> trajectoryList;
+
+    private static Trajectory trajectory;
+
+    private static AutoMode automode;
+
+    private static AutoPath path;
 
     private static List<Rotation2d> headings;
 
     private static Pose2d target;
 
-    private static Rotation2d targetRotation;
-
-    private static List<Pose2d> waypoints = new ArrayList<>();
-
-    /*private static List<Rotation2d> rotationpoints = new ArrayList<>();*/
-
     public TrajectoryToPointAction(double x, double y, Rotation2d r){
-        trajectory = new Trajectory;
+        trajectoryAction = new TrajectoryAction(trajectory, headings);
+        trajectoryList.add(trajectoryAction);
         headings = getAsTrajectoryHeadings();
-        targetRotation = r;
-        target = new Pose2d(x, y, targetRotation);
+        target = new Pose2d(x, y, r);
         //target = new Pose2d (1.0, 1.0, 0.0);
-    }
-
-    public void setWaypoints(){
-        waypoints.add(robotState.fieldToVehicle);
-        waypoints.add(target);
-    }
-
-    /*public List<Pose2d> getPosePoints(){
-        posepoints.add(new Pose2d(50, 50, Rotation2d.fromDegrees(45)));
-        posepoints.add(new Pose2d(1,1,Rotation2d.fromDegrees(0)));
-        return posepoints;
-    }
-
-    public List<Rotation2d> getRotationPoints(){
-        rotationpoints.add()
-    }*/
-
-    public void runTrajectoryToPointAction(){
-        drivetrain.startTrajectory(trajectory, headings);
-        while (drivetrain.getPose() != target){
-            drivetrain.updateTrajectoryPeriodic(trajectory.getTotalTimeSeconds());
-        }
     }
 
     @Override
     protected List<Pose2d> getWaypoints() {
+        List<Pose2d> waypoints = new ArrayList<>();
+        waypoints.add(new Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0)));
+        waypoints.add(target);
         return waypoints;
     }
 
