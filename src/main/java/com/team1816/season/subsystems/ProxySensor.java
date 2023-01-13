66f7b2c42801e19b87b73903ae7a7a9139105fc2
Elena Loucks <elenaloucks@Elenas-MacBook-Air.local>
2023-01-12 19:49:00 -0600

    proxySensor

--- src/main/java/com/team1816/season/subsystems/ProxySensor.java
+++ src/main/java/com/team1816/season/subsystems/ProxySensor.java
@@ -1,12 +1,46 @@
 package com.team1816.season.subsystems;
 
+import com.team1816.lib.Infrastructure;
+import com.team1816.lib.hardware.factory.RobotFactory;
+import com.team1816.lib.subsystems.Subsystem;
+import com.team1816.season.states.RobotState;
 import edu.wpi.first.wpilibj.AnalogInput;
 
-public class ProxySensor extends AnalogInput {
+public class ProxySensor extends Subsystem {
 
-    public ProxySensor(int channel) {
-        super(channel);
+    private static AnalogInput sharp;
+
+    private static AnalogInput sharpFront;
+
+    private static final String NAME = "proxySensor";
+
+    public ProxySensor(String name, Infrastructure inf, RobotState rs) {
+        super(name, inf, rs);
+        sharp = new AnalogInput((int)(Math.round((factory.getConstant("proxySensor")))));
     }
 
+    @Override
+    public void readFromHardware() {
 
+    }
+
+    @Override
+    public void writeToHardware() {
+
+    }
+
+    @Override
+    public void zeroSensors() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public boolean testSubsystem() {
+        return false;
+    }
 }
