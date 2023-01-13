4e1ff2d4f764669f455c1e60397626386325e7de
georgealanzo <georerat@gmail.com>
2023-01-12 19:30:01 -0600

    proxy sensor yaml created and updated

---
+++ src/main/java/com/team1816/season/subsystems/ProxySensor.java
@@ -0,0 +1,12 @@
+package com.team1816.season.subsystems;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class ProxySensor extends AnalogInput {
+
+    public ProxySensor(int channel) {
+        super(channel);
+    }
+
+
+}
