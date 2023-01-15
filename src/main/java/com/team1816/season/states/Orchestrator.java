d778fb580f2ad0cc3f817d848d5a45dd18afa630
Keerthi Kaashyap <keerthik23939@isd273.org>
2023-01-14 20:21:29 -0600

    orchestrator changes

--- src/main/java/com/team1816/season/states/Orchestrator.java
+++ src/main/java/com/team1816/season/states/Orchestrator.java
@@ -42,7 +42,7 @@ public class Orchestrator {
     );
     private final double minAllowablePoseError = factory.getConstant(
         "minAllowablePoseError",
-        0.15
+        0.1
     );
 
     /**
@@ -169,18 +169,10 @@ public class Orchestrator {
         if (
             Math.abs(
                 Math.hypot(
-                    robotState.fieldToVehicle.getX() - newRobotPose.getX(),
-                    robotState.fieldToVehicle.getY() - newRobotPose.getY()
-                )
-            ) <
-                maxAllowablePoseError &&
-                Math.abs(
-                    Math.hypot(
                         robotState.fieldToVehicle.getX() - newRobotPose.getX(),
                         robotState.fieldToVehicle.getY() - newRobotPose.getY()
                     )
-                ) >
-                    minAllowablePoseError
+                ) > minAllowablePoseError
         ) {
             System.out.println(newRobotPose + " = new robot pose");
             drive.resetOdometry(newRobotPose);
