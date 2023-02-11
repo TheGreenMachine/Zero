d2d400e18d589c9e1e8bf0b776fa4f99eb80da89
Elena Loucks <elenaloucks@Elenas-MacBook-Air.local>
2023-02-11 10:15:33 -0600

    collector class test

--- src/main/java/com/team1816/season/subsystems/Collector.java
+++ src/main/java/com/team1816/season/subsystems/Collector.java
@@ -1,50 +1,93 @@
 package com.team1816.season.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.team1816.lib.Infrastructure;
 import com.team1816.lib.hardware.components.motor.IGreenMotor;
 import com.team1816.lib.hardware.components.pcm.ISolenoid;
 import com.team1816.lib.subsystems.Subsystem;
 import com.team1816.season.states.RobotState;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+// Uh-oh... theres no "@Singleton" annotation here! You don't want more than one instance of this being created
+// Oh my, where's the well formatted documentation??
+@Singleton
 public class Collector extends Subsystem {
 
-    private static boolean isCube;
 
-    private static boolean isHolding;
-
-    private final ISolenoid collectorSolenoid;
-
+    //piston is for the switch from cone collecting state to
+    private final ISolenoid collectorPiston;
     private final IGreenMotor intakeMotor;
 
-    private STATE desiredState = STATE.STOP;
+    private double intakeVel;
+
+    private PIVOT_STATE desiredPivotState = PIVOT_STATE.DOWN;
+
+    private COLLECTOR_STATE desiredCollectorState = COLLECTOR_STATE.STOP;
 
     private boolean outputsChanged = false;
 
     private static final String NAME = "collector";
 
+    @Inject
     public Collector (Infrastructure inf, RobotState rs) {
         super(NAME, inf, rs);
-        collectorSolenoid = factory.getSolenoid(NAME, "collectorSolenoid");
+        collectorPiston = factory.getSolenoid(NAME, "collectorSolenoid");
         intakeMotor = factory.getMotor(NAME, "intakeMotor");
-
     }
 
-    public void setDesiredState(STATE state){
-        if (desiredState != state){
-            desiredState = state;
+    public void setDesiredState(PIVOT_STATE pivotState, COLLECTOR_STATE collectorState){
+        if (desiredPivotState != pivotState) {
+            desiredPivotState = pivotState;
+            outputsChanged = true;
+        }
+        if (desiredCollectorState != collectorState) {
+            desiredCollectorState = collectorState;
+            outputsChanged = true;
+        }
+    }
+
+    public void setCollect(boolean setCollect){
+        System.out.println("setCollect method is called");
+        if(setCollect && (desiredCollectorState!=COLLECTOR_STATE.COLLECT)) {
+            desiredCollectorState = COLLECTOR_STATE.COLLECT;
+            System.out.println("desired state is set to true!");
+            outputsChanged = true;
+        }
+        else if(!setCollect) {
+            desiredCollectorState = COLLECTOR_STATE.STOP;
             outputsChanged = true;
         }
     }
     @Override
     public void readFromHardware() {
-
+        intakeVel = intakeMotor.getSelectedSensorVelocity(0);
     }
 
     @Override
     public void writeToHardware() {
         if (outputsChanged) {
             outputsChanged = false;
-
+            switch (desiredPivotState) {
+                case UP:
+                    collectorPiston.set(true);
+                    break;
+                case DOWN:
+                    collectorPiston.set(false);
+                    break;
+            }
+            switch (desiredCollectorState) {
+                case STOP:
+                    intakeMotor.set(ControlMode.Velocity, 0);
+                    break;
+                case COLLECT:
+                    intakeMotor.set(ControlMode.Velocity, factory.getConstant(NAME, "collectTicks"));
+                    break;
+                case FLUSH:
+                    intakeMotor.set(ControlMode.Velocity, factory.getConstant(NAME, "flushTicks"));
+                    break;
+            }
         }
     }
 
@@ -63,9 +106,18 @@ public class Collector extends Subsystem {
         return false;
     }
 
-    public enum STATE {
+    //for cube collecting, the "motor locked" state so we don't pop the cube will have
+    //to be the same as stopping? a hold action and then when its not going it stops? we can't
+    //rotate after getting a cube anyways
+    public enum PIVOT_STATE {
+        UP,
+        DOWN
+    }
+
+    public enum COLLECTOR_STATE {
         STOP,
-        COLLECT_CONE,
-        COLLECT_CUBE,
+        COLLECT,
+        FLUSH
     }
 }
+
