7106855848d085cdb7ed45857b2c948894546354
Rachith Anasuri <43659174+Wirybluehro@users.noreply.github.com>
2023-02-11 12:18:41 -0600

    verify ButtonboardController schemea

--- src/main/java/com/team1816/lib/controlboard/ButtonboardController.java
+++ src/main/java/com/team1816/lib/controlboard/ButtonboardController.java
@@ -13,14 +13,14 @@ public class ButtonboardController extends Controller {
      */
     public ButtonboardController(int port) {
         super(port);
-        mJoystickButtonMap.put(Controller.Button.UP_LEFT, 0);
-        mJoystickButtonMap.put(Controller.Button.UP, 1);
-        mJoystickButtonMap.put(Controller.Button.UP_RIGHT, 2);
+        mJoystickButtonMap.put(Controller.Button.UP_LEFT, 2);
+        mJoystickButtonMap.put(Controller.Button.UP, 5);
+        mJoystickButtonMap.put(Controller.Button.UP_RIGHT, 12);
         mJoystickButtonMap.put(Controller.Button.LEFT, 3);
         mJoystickButtonMap.put(Controller.Button.CENTER, 4);
-        mJoystickButtonMap.put(Controller.Button.RIGHT, 5);
-        mJoystickButtonMap.put(Controller.Button.DOWN_LEFT, 6);
-        mJoystickButtonMap.put(Controller.Button.DOWN, 7);
-        mJoystickButtonMap.put(Controller.Button.DOWN_RIGHT, 8);
+        mJoystickButtonMap.put(Controller.Button.RIGHT, 10);
+        mJoystickButtonMap.put(Controller.Button.DOWN_LEFT, 9);
+        mJoystickButtonMap.put(Controller.Button.DOWN, 11);
+        mJoystickButtonMap.put(Controller.Button.DOWN_RIGHT, 13);
     }
 }
