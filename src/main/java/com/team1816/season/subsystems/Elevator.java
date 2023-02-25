4842333be0220add09cd04b645eb22ca66e96d29
elenaloucks <louckselena@gmail.com>
2023-02-25 09:59:26 -0600

    position extendsion timer

--- src/main/java/com/team1816/season/subsystems/Elevator.java
+++ src/main/java/com/team1816/season/subsystems/Elevator.java
@@ -153,7 +153,8 @@ public class Elevator extends Subsystem {
             () -> {
                 // set it to go down until it hits rubber then just fight against the spring to stay down
                 // that way we don't need to be dead-on for the collect pos
-                extensionMotor.set(ControlMode.PercentOutput, 0);
+                //extensionMotor.set(ControlMode.PercentOutput, 0);
+                extensionMotor.set(ControlMode.Position, minExtension);
                 System.out.println("coasting extension motor");
             }
         );
