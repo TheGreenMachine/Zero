cd4bba3e42c9e8fb0637e9418a61d18dbee2ef6a
Keerthi Kaashyap <keerthik23939@isd273.org>
2023-03-08 10:25:30 -0600

    fix merge conflicts

--- src/main/java/com/team1816/season/auto/modes/ExitCommunityBalanceFeederMode.java
+++ src/main/java/com/team1816/season/auto/modes/ExitCommunityBalanceFeederMode.java
@@ -7,6 +7,7 @@ import com.team1816.lib.auto.actions.TrajectoryAction;
 import com.team1816.lib.auto.actions.WaitAction;
 import com.team1816.lib.auto.modes.AutoMode;
 import com.team1816.season.auto.actions.AutoBalanceAction;
+import com.team1816.season.auto.paths.NodeToChargeStationFeederPath;
 import com.team1816.season.auto.paths.NodeToChargeStationMiddlePath;
 
 import java.util.List;
@@ -15,11 +16,11 @@ public class ExitCommunityBalanceFeederMode extends AutoMode {
     //Does NOT exit community
 
     public ExitCommunityBalanceFeederMode() {
-        super(List.of(new TrajectoryAction(new NodeToChargeStationMiddlePath())));
+        super(List.of(new TrajectoryAction(new NodeToChargeStationFeederPath())));
     }
 
     public ExitCommunityBalanceFeederMode(Color color) {
-        super(List.of(new TrajectoryAction(new NodeToChargeStationMiddlePath(color))));
+        super(List.of(new TrajectoryAction(new NodeToChargeStationFeederPath(color))));
     }
 
     @Override
