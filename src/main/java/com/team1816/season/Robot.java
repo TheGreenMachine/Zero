2a55ff850ed1ab2d1c5ec9a97a94bd597969660f
Keerthi Kaashyap <keerthik23939@isd273.org>
2023-03-11 11:00:41 -0600

    verification changes, remove sout

--- src/main/java/com/team1816/season/Robot.java
+++ src/main/java/com/team1816/season/Robot.java
@@ -476,9 +476,8 @@ public class Robot extends TimedRobot {
                                         System.out.println("Auto Score action started!");
                                         AutoScoreMode mode = new AutoScoreMode(desireCube, level);
                                         autoScoreThread = new Thread(mode::run);
-                                        ledManager.indicateStatus(LedManager.RobotStatus.AUTO_SCORE, LedManager.ControlState.BLINK);
+                                        ledManager.indicateStatus(LedManager.RobotStatus.ON_TARGET, LedManager.ControlState.BLINK);
                                         autoScoreThread.start();
-                                        System.out.println("Auto Scoring ended");
                                     }
                                 } else {
                                     autoScoreThread.stop();
