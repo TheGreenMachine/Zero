6fa3af9c4b11376c8734bf461a3d57311d1f04d9
Keerthi Kaashyap <keerthik23939@isd273.org>
2023-03-22 19:23:46 -0500

    minute updates to field targets based on wpi json

--- src/main/java/com/team1816/season/configuration/FieldConfig.java
+++ src/main/java/com/team1816/season/configuration/FieldConfig.java
@@ -24,16 +24,16 @@ public class FieldConfig {
              */
 
             /* Red Alliance Side */
-            put(1, new Pose3d(new Translation3d(15.453, 1.282, 0.470), new Rotation3d(0, 0, Math.PI))); // Red Edge Grid
-            put(2, new Pose3d(new Translation3d(15.453, 2.959, 0.470), new Rotation3d(0, 0, Math.PI))); // Red Co-Op Grid
-            put(3, new Pose3d(new Translation3d(15.453, 4.635, 0.470), new Rotation3d(0, 0, Math.PI))); // Red Feeder Edge Grid
-            put(4, new Pose3d(new Translation3d(16.106, 7.126, 0.700), new Rotation3d(0, 0, Math.PI))); // Blue Feeder Station
+            put(1, new Pose3d(new Translation3d(15.514, 1.072, 0.470), new Rotation3d(0, 0, Math.PI))); // Red Edge Grid
+            put(2, new Pose3d(new Translation3d(15.514, 2.748, 0.470), new Rotation3d(0, 0, Math.PI))); // Red Co-Op Grid
+            put(3, new Pose3d(new Translation3d(15.514, 4.424, 0.470), new Rotation3d(0, 0, Math.PI))); // Red Feeder Edge Grid
+            put(4, new Pose3d(new Translation3d(16.178, 6.750, 0.700), new Rotation3d(0, 0, Math.PI))); // Blue Feeder Station
 
             /* Blue Alliance Side */
-            put(5, new Pose3d(new Translation3d(0.354, 7.126, 0.700), new Rotation3d(0, 0, 0))); // Red Feeder Station
-            put(6, new Pose3d(new Translation3d(1.007, 4.635, 0.470), new Rotation3d(0, 0, 0))); // Blue Feeder Edge Grid
-            put(7, new Pose3d(new Translation3d(1.007, 2.959, 0.470), new Rotation3d(0, 0, 0))); // Blue Co-Op Grid
-            put(8, new Pose3d(new Translation3d(1.007, 1.282, 0.470), new Rotation3d(0, 0, 0))); // Blue Edge Grid
+            put(5, new Pose3d(new Translation3d(0.361, 6.750, 0.700), new Rotation3d(0, 0, 0))); // Red Feeder Station
+            put(6, new Pose3d(new Translation3d(1.027, 4.424, 0.470), new Rotation3d(0, 0, 0))); // Blue Feeder Edge Grid
+            put(7, new Pose3d(new Translation3d(1.027, 2.748, 0.470), new Rotation3d(0, 0, 0))); // Blue Co-Op Grid
+            put(8, new Pose3d(new Translation3d(1.027, 1.072, 0.470), new Rotation3d(0, 0, 0))); // Blue Edge Grid
 
             /* Test */
             put(51, new Pose3d(new Translation3d(Constants.fieldCenterX, Constants.fieldCenterY, 0.1), new Rotation3d()));
