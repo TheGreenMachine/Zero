470e71e8b9fba0e5bfbab152bccdd798f5ff78d3
Keerthi Kaashyap <keerthik23939@isd273.org>
2023-03-28 14:37:51 -0500

    tmp

--- src/main/java/com/team1816/season/subsystems/Collector.java
+++ src/main/java/com/team1816/season/subsystems/Collector.java
@@ -102,7 +102,7 @@ public class Collector extends Subsystem {
             Constants.kCANTimeoutMs
         );
 
-        intakeMotor.configOpenloopRamp(0.1, Constants.kCANTimeoutMs);
+        intakeMotor.configOpenloopRamp(0.25, Constants.kCANTimeoutMs);
 
         allowablePivotError = factory.getPidSlotConfig(NAME, "slot1").allowableError;
 
