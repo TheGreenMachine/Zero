57cb2fdc3565703634368da81041401d33be41ad
tagciccone <tagciccone@gmail.com>
2023-11-16 17:19:06 -0600

    Revert "exp3"
    This reverts commit 212a31b73de34ce104aea32153155e72f66b260d.

--- src/main/java/com/team1816/lib/hardware/factory/RobotFactory.java
+++ src/main/java/com/team1816/lib/hardware/factory/RobotFactory.java
@@ -261,8 +261,6 @@ public class RobotFactory {
         var canCoder = getCanCoder(subsystemName, name);
 
         return new SwerveModule(subsystemName, moduleConfig, canCoder);
-
-        //temp
     }
 
     public CANCoder getCanCoder(String subsystemName, String name) {
